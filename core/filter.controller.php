272bc6d94c088869ea60c0d5da9383e249d7b980
RanMax <ChernovMaxim@mail.ru>
2015-10-13 15:27:17 +0300

    Небольшие изменения

---
+++ core/filter.controller.php
@@ -0,0 +1,20 @@
+﻿<?php
+class FilterController {
+	public function execute($params){
+		$model = Model::getModel();
+		$filterId = $params[5048];
+		$filters2[5095] = $filterId;
+		$filter = $model->getResource(163,$filters2);
+		//print_r($filter);
+		$filters[5095] = '%COLUMN%='.$filterId;
+		$fields = $model->getResource(162,$filters);
+		echo ' <script language ="JavaScript">data[5095]['.$filterId.'] = '.$filter[0]->items[5094].'; </script>';
+		echo "<select onchange='editFilter(".$filterId.",this.options[this.selectedIndex].value); sendData(2316);'>";
+		foreach($fields as $field){
+			if ($field->items[5048] == $filter[0]->items[5094]) $selected = 'selected'; else $selected = '';
+			echo "<option value = '".$field->items[5048]."' ".$selected.">".$field->items[501].'</option>';
+		}
+		echo '</select>';
+	}
+}
+?>
---
+++ core/set.controller.php
@@ -0,0 +1,48 @@
+<?php
+class SetController {
+	public function execute($params){
+		$filters=$params[5095];
+		$model = Model::getModel();
+		$action = $model->getAction($params[5058]); //5058.Действие
+		//if (empty($params[5065])) {
+		$formId = $action->items[5065];
+		$columns = $model->getColumns2($formId);
+		if ($params[5091] == 1) $id = 1; else $id = null; //5091.Флаг поиска
+		$table = $model->getDataSet($columns, $formId,$id,$filters);
+		//$val = $model->getShowData($_REQUEST['elem']);
+		
+		echo '<div class="table-responsive" id="dop_form_interface"><table class="table table-striped table-hover table-condensed"><thead><tr>';
+        // Выводим результаты в html
+		$colNum = 0;
+        foreach ($table->cols as $col_value) {
+		  if ($col_value->type == 0) {
+			  //$elemId = $model->getEShowElement($col_value->domain);
+			  //if (!empty($elemId)) $link[$colNum] = $elemId;
+		  }
+          echo "<th>".$col_value->name.$link[$col_value->alias]."</th>";
+		  $colNum++;
+        }
+        echo "</tr></thead><tbody>";
+        
+        if (!empty($table->data)) foreach ($table->data as $val) {
+            echo "<tr>";
+			$colNum = 0;
+            foreach ($val as $col_value) {
+				$params[5055] = $table->cols[$colNum]->domain;
+				$viewer = $model->getViewer($table->cols[$colNum]->viewer,$params,$this);
+				//echo $viewer;
+	  
+				echo '<td>'.$viewer->show($col_value,$params).'</td>';
+				/*
+				if (!empty($link[$colNum])) $col_value->value = '<a href="index.php?action=2315&id='.$col_value->value.'">'.$col_value->value.'</a>';
+                echo "<td>".$col_value->value."</td>";
+				*/
+				$colNum++;
+            }
+            echo "</tr>";
+        }
+        
+        echo "</tbody></table></div>";
+	}
+}
+?>
