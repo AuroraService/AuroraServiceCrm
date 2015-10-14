f93c399cd78db8251f1017b026fa035b7e9df47f
mcKaen <kaen337@gmail.com>
2015-10-14 12:23:54 +0300

    no message

--- core/ajax/interface.php
+++ core/ajax/interface.php
@@ -6,6 +6,10 @@ $model = Model::getModel();
 $action = $_REQUEST['action'];
 $domain = $_REQUEST['domain']; 
 $id =  $_REQUEST['id'];
+$json = json_decode($_POST['data'], true);
+if ($action==""){
+	$action=$json[5058];
+}
 //echo "123";
 //$json = json_decode($_POST['data'], true);
 //if (!empty($json[5058])) $action = $json[5058];
@@ -15,7 +19,6 @@ switch ($action) {
         eshow($id);
         break;
 	case '2316':
-		$json = json_decode($_POST['data'], true);
 		$params[5058] = $json[5058];
 		$params[5095] = $json[5095];
 		$f1[5058] = '%COLUMN%='.$params[5058];
@@ -36,9 +39,8 @@ switch ($action) {
 		break;
 	case '2333':
 		//echo "123";
-		$json = json_decode($_POST['data'], true);
 		//echo $json[5055][0];
-		$domain = $model->getResProperty($json[5055][0],507); //507.�������������
+		$domain = $model->getResProperty($json[5055][0],507); //507.Ïðåäñòàâëåíèå
 		$model->find($json[5091][0], $domain);
 
 		require_once('core/show.controller.php');
@@ -54,12 +56,14 @@ switch ($action) {
 		break;
 	case '2334':
         //eshow($id);
-		$json = json_decode($_POST['data'], true);
 		//file_put_contents ('log', 'stepx'.$json[5048][0],FILE_APPEND);
 		$resource2 = new Resource2($json);
 		if (!empty($json[5048][0])) $model->update($resource2);
 		else $model->insert($resource2);
 		//entityEdit($json,$model);
+        break;
+    case '2336':
+        echo search($json);
         break;
 	return 0;
 }
@@ -71,6 +75,19 @@ function eshow($id){
 	$params[5058] = 2315;
 	//$params[5014] = $elemId;
 	$controller->execute($params);
+}
+
+function search($json){
+$rets='<div class="popup_btn_close" onclick="click_close_btn()"></div>
+       <input type="text" class="form-control" placeholder="Поиск" style="margin-bottom: 10px;">
+       <div class="list-group" id="find_list_div" style="overflow-y: scroll; max-height:'.$json[9091].'; border: 1px solid rgb(204,204,204);">';
+    $i=0;
+    while($i<100){ 
+      $i++;
+      $rets=$rets.'<a href="#" class="list-group-item popup_find_list_item" onclick="popup_list_change_item(this)" id="popup_list<? echo $i; ?>">Тестовая запись</a>'; 
+    }
+$rets=$rets.'</div>';
+return $rets;
 }
 
 
