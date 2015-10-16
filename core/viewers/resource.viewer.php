50cdc5cde65c1b358f770a8fa38a06a99a69c400
mcKaen <kaen337@gmail.com>
2015-10-16 12:02:47 +0300

    Рабочее окно поиска

--- core/viewers/resource.viewer.php
+++ core/viewers/resource.viewer.php
@@ -11,11 +11,11 @@ class ResourceViewer{
 		$this->params = $params;
 	}
 	public function show($cell, $params){
-		$id = $params[5082].'-'.$params[5088];
+		$id = $params[5082].'_'.$params[5088];
 		global $var_pth_template;
 		$link = "&id=".$cell->id;
 		$link1='<a href="?action=2315'.$link.'" class="hrf_nmrg" style="margin-left: 10px;"><span class="glyphicon glyphicon-share-alt" style="color:black"></span></a>';
-		$link2='<a href="#" class="hrf_nmrg interface_edit" id="select_form" domain="'.$this->params[5055].'" selectid="'.$cell->id.'" win_width="600" win_height="300"><span class="glyphicon glyphicon-pencil" style="color:black"></span></a>';
+		$link2='<a href="#" class="hrf_nmrg interface_edit" id="select_form" domain="'.$this->params[5055].'" selectid="'.$cell->id.'" item-id="'.$id.'" win_width="600" win_height="300"><span class="glyphicon glyphicon-pencil" style="color:black"></span></a>';
 		$link3='<a class="hrf_nmrg" onclick="editField(\''.$id.'\', \'\');"><span class="glyphicon glyphicon-remove" style="color:black"></span></a>';
 		$val='<div class="res_value_link" id = "'.$id.'">'.$cell->value.'</div>';
 		return '<div style="margin-bottom: 15px" onclick="load_data(2315, '.$this->params[5055].', '.$cell->id.')">'.$val.'</div>'.$link1.$link2.$link3;
