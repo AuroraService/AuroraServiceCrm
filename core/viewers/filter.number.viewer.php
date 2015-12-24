7aa6b65263ee3de0cc8ec482a296f4072f6ff71f
AuroraService <ChernovMaxim@mail.ru>
2015-12-24 17:19:14 +0300

    Отображатели фильтров

---
+++ core/viewers/filter.number.viewer.php
@@ -0,0 +1,19 @@
+<?php
+class FilterNumberViewerFabrica{
+    public function getViewer($params, $model){
+        return new FilterNumberViewer($params, $model);
+    }
+}
+class FilterNumberViewer{
+    public $model;
+    public $items;
+    public function __construct($params, $model){
+        $this->model = $model;
+    }
+    public function show($cell, $params){
+        // $cell->value
+        if ($params[5099]!="") $params[5099]=' active';
+        $result[1]='<a href="#" class="list-group-item'.$params[5099].'"><img src="icons/kuper.png" height="15" style="float: left; margin-right: 3px;"></img><h5 class="list-group-item-heading">'.$cell->items[50139].'</h5></a>';
+        return $result[1];
+    }
+}
